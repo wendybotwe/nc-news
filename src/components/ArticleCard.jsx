55b75952a43c497fe0f3b76415fcc25896985c3e
wendybotwe <wendy.botwe@hotmail.co.uk>
2025-05-20 13:13:52 +0100

    task 6 view comments by article completed

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
-import { getArticleById } from "../api"
+import { getArticleById, getCommentsByArticleId } from "../api"
 
 function ArticleCard() {
   const { article_id } = useParams();
   const [article, setArticle] = useState(null);
+  const [comments, setComments] = useState([])
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
 
   useEffect(() => {
     setLoading(true);
-    getArticleById(article_id)
-      .then((data) => {
-        setArticle(data);
+    Promise.all([
+      getArticleById(article_id),
+      getCommentsByArticleId(article_id)
+    ])
+      .then(([articleData, commentsData]) => {
+        setArticle(articleData);
+        setComments(commentsData)
         setLoading(false);
       })
       .catch((err) => {
@@ -23,7 +28,7 @@ function ArticleCard() {
   }, [article_id]);
 
   if (loading) return <p>Loading an excellent article for you...</p>
-  if (error) return <p>Sorry, article could not be loaded.</p>
+  if (error) return <p>Sorry, article or comments could not be loaded.</p>
   if (!article) return null;
 
   return (
@@ -34,6 +39,15 @@ function ArticleCard() {
       <p><strong>Topic:</strong>{article.topic}</p>
       <p><strong>Published:</strong>{new Date(article.created_at).toLocaleDateString()}</p>
       <article>{article.body}</article>
+      <h3>Comments</h3>
+      {comments.length === 0 ? (<p>No comments yet</p>) : (<ul className="comments-list">
+        {comments.map((comment) => (
+          <li key={comment.comment_id}>
+            <p>On {new Date(article.created_at).toLocaleDateString()},<strong> {comment.author}</strong> said</p>
+            <p>{comment.body}</p>
+          </li>
+        ))}
+      </ul>)}
     </div>
   )
 }
